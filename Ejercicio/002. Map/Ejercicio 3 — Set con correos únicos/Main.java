97b9421d3485a9d25ff93c7a0d79b71f02e15dac
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-04-10 03:23:45 -0400

    java

---
+++ Ejercicio/002. Map/Ejercicio 3 — Set con correos únicos/Main.java
@@ -0,0 +1,43 @@
+package Collections_Framework.Set;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> emails = new HashSet<>(
+                List.of(
+                        "rodriguez299@gmail.com",
+                        "martinez47@gmail.com",
+                        "jason229@gmail.com",
+                        "martinez47@gmail.com",
+                        "rodriguez299@gmail.com",
+                        "ada225@gmail.com"
+                )
+        );
+
+        // Imprimir todo el Set
+        System.out.println("Correos únicos:");
+        for (String email : emails) {
+            System.out.println(email);
+        }
+
+        // Verificar si un correo existe
+        System.out.println("\n¿Existe jason229@gmail.com? " + emails.contains("jason229@gmail.com"));
+
+        // Eliminar un correo
+        emails.remove("martinez47@gmail.com");
+
+        // Recorrer todo el Set con un for
+        System.out.println("\nCorreos después de eliminar uno:");
+        for (String email : emails) {
+            System.out.println(email);
+        }
+
+        // Mostrar tamaño final
+        System.out.println("\nTamaño real: " + emails.size());
+
+
+    }
+}
