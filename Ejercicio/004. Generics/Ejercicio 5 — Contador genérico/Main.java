89df9930ebae2706064134456fdea24256ee392b
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-04-19 14:10:30 -0400

    java

---
+++ Ejercicio/004. Generics/Ejercicio 5 — Contador genérico/Main.java
@@ -0,0 +1,31 @@
+package Collections_Framework.Box.Ejercicio_14_Contador_generic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static <T> Map<T, Integer> contarElement(List<T> lista){
+        Map<T, Integer> contador = new HashMap<>();
+
+        for(T list: lista) {
+
+            int valorActual = 0;
+            if (contador.containsKey(list)) {
+                valorActual = contador.get(list);
+
+                contador.put(list, valorActual+1);
+            } else {
+                contador.put(list,1);
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(1,2,3,3,3));
+
+        System.out.println(contarElement(numbers));
+    }
+}
